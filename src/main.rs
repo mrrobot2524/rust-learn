5b8354e71953e6af139eecb2336d2c40a59b2d46
mr-robot <alenjon2594@gmail.com>
2024-02-07 15:40:32 +0500

    practice

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    // Positive and Negative Integers
+        // let mut age: i8 = 98;
+        // age = -99;
+        // println!("My age is {}", age);
+    
+    // Only positive integers
+        // let mut num2:u8 = 25;
+        // println!("My only positive number: {}", num2);
+    // Float
+        // let mut num1:f32 = 0.25;
+        // num1 = 657.45;
+        
+        // println!("My num is {}", num1);
+
+    // Strings
+        // let name = String::from("Khabib");
+        // println!("My name is {}", name);
+
+    // Char
+        // let sybmbol:char = 'F';
+        // println!("{}", sybmbol);
+
+
+    // Bool logic (true, false)
+        // let logic: bool = true;
+        // let logic1 = false;
+
+        // println!("{}, {}", logic, logic1); 
+
+
+    // Comments
+        println!("Hello Rust!")
+}
